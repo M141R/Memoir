d97e426d73be75f8515d287957352ceec7cf5b9f
Mihir <68944128+M141R@users.noreply.github.com>
2024-05-05 11:09:09 +0000

    Added calendar and highlighting in calendar

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import current_user, login_required
 from .models import Post, User
 from . import db
+from datetime import datetime, timedelta
+import calendar
 
 views = Blueprint('views', __name__)
 
@@ -50,20 +52,38 @@ def delete_post(id):
         
     return redirect(url_for('views.index'))
 
-@views.route("/posts/<username>")
+@views.route("/posts/<username>", defaults={'year': None, 'month': None, 'day': None})
+@views.route("/posts/<username>/<int:year>/<int:month>/<int:day>")
 @login_required
-def posts(username):
+def posts(username, year, month, day):
     user = User.query.filter_by(username=username).first()
+    now = datetime.now()
 
     if not user:
         flash('No user with that username exists', category='error')
         return redirect(url_for('views.index'))
     
-    posts = Post.query.filter_by(author=user.id).all()
-    return render_template("posts.html", user=current_user, posts=posts, username=username)
+    if year and month and day:
+        date = datetime(year, month, day)
+        next_day = date + timedelta(days=1)
+        posts = Post.query.filter(Post.date_created >= date, Post.date_created < next_day, Post.author == user.id).all()
+    else:
+        posts = Post.query.filter_by(author=user.id).all()
+
+    if not year or not month:
+
+        year = now.year
+        month = now.month
+        
+    post_dates = [post.date_created.date() for post in Post.query.filter_by(author=current_user.id).all()]
+    return render_template("posts.html", user=current_user, posts=posts, username=username, year=now.year, month=now.month, calendar=calendar, post_dates=post_dates, datetime=datetime)
 
 @views.route('/post/<id>')
 @login_required
 def post_detail(id):
     post = Post.query.get(id)
     return render_template('post_page.html', post=post,user=current_user)
+
+
+
+
